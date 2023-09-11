15ee565021aac3f6e1fd0f154cb870bd3947e821
wenshuyu <wenhsuyu991@hellobike.com>
2023-09-11 14:13:15 +0800

    1

--- T61Host/main.go
+++ T61Host/main.go
@@ -5,8 +5,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
-
 	var m_cfg = new_config()
 
 	port_names := m_cfg.get_post_names()
@@ -14,17 +12,13 @@ func main() {
 	m_cfg.set_baudrate(115200)
 	m_cfg.set_port(port_names[0])
 
-	fmt.Printf("baud:%d\n", m_cfg.mode.BaudRate)
-	fmt.Printf("port:%v\n", m_cfg.port_name)
-
 	port := new_port(&m_cfg)
-	fmt.Printf("%v\n", port)
 
 	len_wr, err := port.Write([]byte("hello world"))
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(len_wr)
+	fmt.Println("write length:",len_wr)
 
 	port.Drain()
 
@@ -35,7 +29,6 @@ func main() {
 			fmt.Println(err)
 		}
 		if n == 0{
-			fmt.Println("EOF")
 			break
 		}
 		fmt.Printf("%v",string(rd_buf[:n]))
